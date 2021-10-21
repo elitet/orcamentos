ded5c133912cfe1ae3da4f92511d88dee6a9e945
Matheus Marques <matheus_marquest@outlook.com>
2021-10-20 22:19:13 -0300

    Criaçao da estrutura principal

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,34 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+$( document ).ready(function() {
+        var getParentAnchor = function (element) {
+        while (element !== null) {
+        if (element.ariaLabel && element.tagName.toUpperCase() === "A") {
+            return element;
+        }
+        element = element.parentNode;
+        }
+        return null;
+    };
+    
+    document.querySelector("body").addEventListener('click', function(e) {
+        e.preventDefault();
+        var anchor = getParentAnchor(e.target);
+        
+        if(anchor !== null) {
+
+            const existingElements = document.querySelectorAll(".lMenu");
+            const elmSel = Array.from(existingElements).filter(chapter => chapter.classList.contains("active"))
+
+            elmSel[0].classList.remove("active")
+            anchor.classList.add("active");
+            pages(anchor.ariaLabel)
+        }
+    }, false);
+});
+
+let pages = (page) => {
+    $('#main').load('/pages/'+page+'.html');
+}
